1bc9aa2294d98a4acecab09dbb03de1de61e3de0
andrewpfunk <andrewpfunk@gmail.com>
2025-06-30 17:40:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -245,10 +245,9 @@ class Controller {
 
 const app = new Controller(new Model(), new View());
 
-// TODO call loadTodos and add a todo
-let result = await fetch('/.netlify/functions/loadTodos', {
-  method: 'GET',
-});
+// TODO call loadTodos and add the todo
+let result = await fetch('/.netlify/functions/loadTodos');
+
 
 const localStorageSetHandler = async function(e) {  
   let result = await fetch('/.netlify/functions/saveTodos', {
