1b89269c28147de4eda4007691eb55533f8d320e
andrewpfunk <andrewpfunk@gmail.com>
2025-07-05 17:18:28 -0400

    try using _default scope and collection

--- netlify/functions/loadTodos/loadTodos.js
+++ netlify/functions/loadTodos/loadTodos.js
@@ -23,12 +23,14 @@ const handler = async (event) => {
 
   try {
 
+    // try using _default scope and collection
     const cluster = await couchbaseClientPromise
     const bucket = cluster.bucket(BUCKET)
-    const scope = bucket.scope(BUCKET)
-    const collection = scope.collection(BUCKET)
+    // const scope = bucket.scope(BUCKET)
+    // const collection = scope.collection(BUCKET)
 
-    const results = await collection.get(BUCKET)
+    // const results = await collection.get(BUCKET)
+    const results = await cluster.get(BUCKET)
 
     return {
       statusCode: 200,
--- netlify/functions/saveTodos/saveTodos.js
+++ netlify/functions/saveTodos/saveTodos.js
@@ -22,12 +22,15 @@ const handler = async (event) => {
   }
 
   try {
+
+    // try using _default scope and collection
     const cluster = await couchbaseClientPromise
     const bucket = cluster.bucket(BUCKET)
-    const scope = bucket.scope(BUCKET)
-    const collection = scope.collection(BUCKET)
+    // const scope = bucket.scope(BUCKET)
+    // const collection = scope.collection(BUCKET)
 
-    const result = await collection.upsert('todos', event.body)
+    // const result = await collection.upsert('todos', event.body)
+    const result = await cluster.upsert('todos', event.body)
 
     return {
       statusCode: 200,
