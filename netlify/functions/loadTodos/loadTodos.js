af51c1e9add8258859195a8873246a05909137ff
andrewpfunk <andrewpfunk@gmail.com>
2025-07-05 13:10:32 -0400

    fiddling with response format

--- netlify/functions/loadTodos/loadTodos.js
+++ netlify/functions/loadTodos/loadTodos.js
@@ -30,11 +30,13 @@ const handler = async (event) => {
 
     const results = await cluster.query(`SELECT ${BUCKET} from ${BUCKET}.${BUCKET}.${BUCKET}`)
     // SELECT todos from todos.todos.todos is returning an array, containing one object, with key 'todos' and value what we're after
+    // {"rows":[{"todos":[{"complete":true,"id":1,"text":"Create a serverless function"}]}],"meta":{"requestId":"ff181a9e-ab19-4f93-b16c-f2d943defc99","clientContextId":"fa0619-3963-e444-bac7-dfd084c5d6eea7","status":"success","signature":{"todos":"json"},"warnings":[]}}
     // const results = [{id: 1, text: "Create a serverless function", complete: false}];
+    const whatIwant = results.rows.todos
 
     return {
       statusCode: 200,
-      body: JSON.stringify(results),
+      body: JSON.stringify(whatIwant),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
